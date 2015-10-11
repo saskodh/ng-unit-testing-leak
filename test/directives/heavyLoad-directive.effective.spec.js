42f4dadf6164065b34e3223de2e32be1594afe75
Sashe Klechkovski <Sashe.Klechkovski@netcetera.com>
2015-10-11 15:30:58 +0200

    Memory leak problem solved: - call .remove() on compiled elements to prevent DOM elements leak - always null local variables in the tests in afterEach function to prevent memory leak because of JavaScript closures referenced from inside Jasmine

---
+++ test/directives/heavyLoad-directive.effective.spec.js
@@ -0,0 +1,59 @@
+'use strict';
+
+(function () {
+
+  var testDefinition = function () {
+
+    var suite = {};
+
+    beforeEach(module('app'));
+
+    beforeEach(inject(function ($rootScope, $compile, heavyLoad) {
+      suite.$rootScope = $rootScope;
+      suite.$compile = $compile;
+      suite.heavyLoad = heavyLoad;
+      suite.$scope = $rootScope.$new();
+
+      spyOn(suite.heavyLoad, 'getHeavyString').and.callThrough();
+      spyOn(suite.heavyLoad, 'getHeavyObject').and.callThrough();
+      spyOn(suite.heavyLoad, 'getHeavyList').and.callThrough();
+    }));
+
+    // NOTE: cleanup
+    afterEach(function () {
+
+      // NOTE: prevents DOM elements leak
+      suite.element.remove();
+
+      // NOTE: prevents memory leaks because of JavaScript closures created for the jasmine syntax
+      // (beforeEach, afterEach, beforeAll, afterAll, it..).
+      suite = null;
+    });
+
+    suite.compileDirective = function (template) {
+      suite.element = suite.$compile(template)(suite.$scope);
+      suite.directiveScope = suite.element.isolateScope();
+      suite.directiveController = suite.element.controller('heavyLoad');
+    };
+
+    it('should compile correctly', function () {
+      // given
+      var givenTemplate = '<div heavy-load></div>';
+
+      // when
+      suite.compileDirective(givenTemplate);
+
+      // then
+      expect(suite.directiveScope.title).toBeDefined();
+      expect(suite.directiveScope.items).toBeDefined();
+      expect(suite.heavyLoad.getHeavyString).toHaveBeenCalled();
+      expect(suite.heavyLoad.getHeavyList).toHaveBeenCalled();
+    });
+  };
+
+  // define multiple suits with the same definition just for showcase
+  for (var i = 0; i < 3000; i += 1) {
+    describe('heavyLoad effective directive #' + i, testDefinition);
+  }
+
+})();
